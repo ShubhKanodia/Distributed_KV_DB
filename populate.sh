cdc6300e8239a8ead49f347d0b6eacd8305e2b40
Shubh Kanodia <pes1202100702@pesu.pes.edu>
2024-07-19 03:55:52 +0530

    read replicas added.

--- populate.sh
+++
@@ -1,6 +0,0 @@
-for shard in localhost:8080 localhost:8081; do
-    echo $shard
-    for i in {1..1000}; do
-        curl "http://$shard/set?key=key-$RANDOM&value=value-$RANDOM"
-    done
-done
--- test.sh
+++
@@ -1,3 +0,0 @@
-for i in {1..100}; do
-    curl "http://localhost:8081/get?key=key-$i"
-done
